3aff11a7c04209dcff20252f473d2c310251bf94
ashishank01 <ashishank.20097@gmail.com>
2016-05-15 02:11:02 +0530

    First Commit

---
+++ src/test/java/testScript/Test1.java
@@ -0,0 +1,15 @@
+package testScript;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Ashish to Eclipse");
+
+	}
+
+}
