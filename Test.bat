ec4605ccfe96498cf404a73516d2e7a9c2284a83
ashishank01 <ashishank.20097@gmail.com>
2016-05-15 21:26:48 +0530

    mvn Test Complete. Commit.

---
+++ Test.bat
@@ -0,0 +1,3 @@
+E:
+cd E:\Selenium\Workspace\AutomationFramework
+mvn clean test site org.reportyng:reporty-ng:1.2:reportyng -Dmaven.test.failure.ignore=true
---
+++ src/test/java/utility/ListenerClass.java
@@ -0,0 +1,75 @@
+package utility;
+
+//import org.testng.IClass;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class ListenerClass extends TestListenerAdapter {
+
+	@Override
+	public void onTestStart(ITestResult tr) {
+		log("--------------Test Started--------------");
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult tr) {
+
+		log("Test Name : '" + tr.getName() + "' - PASSED");
+
+		// This will print the class name in which the method is present
+		log("Test Class : " + tr.getTestClass());
+		
+		// This will print the method name
+		log("Test Method : " + tr.getMethod());
+
+		// This will print the priority of the method.
+		// If the priority is not defined it will print the default priority as 'o'
+		log("Test Priority : " + tr.getMethod().getPriority());
+
+		System.out.println("-----------------------------------------");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult tr) {
+
+		log("Test Name : '" + tr.getName() + "' - FAILED");
+
+		// This will print the class name in which the method is present
+		log("Test Class : " + tr.getTestClass());
+		
+		// This will print the method name
+		log("Test Method : " + tr.getMethod());
+
+		// This will print the priority of the method.
+		// If the priority is not defined it will print the default priority as 'o'
+		log("Test Priority : " + tr.getMethod().getPriority());
+
+		System.out.println("-----------------------------------------");
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult tr) {
+		log("Test Name : '" + tr.getName() + "' - SKIPPED");
+
+		// This will print the class name in which the method is present
+		log("Test Class : " + tr.getTestClass());
+		
+		// This will print the method name
+		log("Test Method : " + tr.getMethod());
+
+		// This will print the priority of the method.
+		// If the priority is not defined it will print the default priority as 'o'
+		log("Test Priority : " + tr.getMethod().getPriority());
+
+		System.out.println("-----------------------------------------");
+	}
+
+	private void log(String methodName) {
+		System.out.println(methodName);
+	}
+
+	//private void log(IClass testClass) {
+	//	System.out.println(testClass.getClass());
+	//}
+
+}
